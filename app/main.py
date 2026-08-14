b26513bc6b997e664e6ca2ee10eaf112da8ac128
Tocny <marcoarubior@ciencias.unam.mx>
2026-08-13 20:51:49 -0600

    Feat[security]: config de cors, para cuando conectemos frontend.

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ import logging
 import time
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.middleware import SlowAPIMiddleware
@@ -56,6 +57,22 @@ app = FastAPI(
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
+
+# Config de cors
+ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
+if settings.environment == "development":
+    ALLOWED_ORIGINS.append("*")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 #Config rutas
 app.include_router(health.router)
